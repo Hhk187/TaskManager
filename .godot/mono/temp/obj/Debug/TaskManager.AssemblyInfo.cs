371964cbd3a332e6e37e213670b4d8fb86880c9c
Hhk187 <114492671+Hhk187@users.noreply.github.com>
2026-03-16 23:46:37 +0100

    updated to gdscript

--- .godot/mono/temp/obj/Debug/TaskManager.AssemblyInfo.cs
+++ .godot/mono/temp/obj/Debug/TaskManager.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7896a4927d5eb53a4d5169d89b74f13b3b2e789")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09c72734cd576881cd7c0dc00a68927ba6bf3d2f")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
