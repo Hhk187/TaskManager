a7896a4927d5eb53a4d5169d89b74f13b3b2e789
Hhk187 <114492671+Hhk187@users.noreply.github.com>
2026-03-16 23:05:28 +0100

    checkpoint

--- Scripts/UIButton.cs
+++ Scripts/UIButton.cs
@@ -1,23 +1,22 @@
 using Godot;
 using System;
-
-public partial class UIButton : Button
+[Tool]
+public partial class UIButton : PanelContainer
 {
-	[Export]
-	PackedScene link;
-
+	
+	[Export] String Text;
+	[Export] Label Label;
+	[Export] TextureButton Button;
+	
+	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		Pressed += _onPressed;
+		Button = GetNode<TextureButton>("TextureButton");
+		Label = GetNode<Label>("Label");
 	}
 
-	private void  _onPressed()
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
 	{
-		if (link != null)
-		{
-			GetTree().ChangeSceneToPacked(link);
-		}
 	}
-
-
 }
